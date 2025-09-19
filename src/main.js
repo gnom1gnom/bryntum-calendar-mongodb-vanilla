50d9f1fab1a5d089bd435aa0599e08da16a3a595
Arsalan Khattak <me@khattak.dev>
2025-09-19 15:50:36 +0500

    inital

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import './style.css';
+import { Calendar } from '@bryntum/calendar';
 
-document.querySelector('#app').innerHTML = `
-  <h2>
-    Bryntum Calendar App with MongoDB
-  </h2>
-`
+const calendar = new Calendar({
+
+    appendTo : 'app',
+    date : '2025-10-05',
+
+    resources : [
+        {
+            id         : 1,
+            name       : 'Default Calendar',
+            eventColor : 'green'
+        }
+    ],
+    events : [
+        {
+            id         : 1,
+            name       : 'Meeting',
+            startDate  : '2025-10-05T10:00:00',
+            endDate    : '2025-10-05T11:00:00',
+            resourceId : 1
+        }
+    ]
+});
