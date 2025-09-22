e9a1ca4ea899734722f9802d3bdf925a6b7e7372
Arsalan Khattak <me@khattak.dev>
2025-09-22 12:03:17 +0500

    instructions

---
+++ README.md
@@ -0,0 +1,12 @@
+## Bryntum Calendar with MongoDB
+
+This is a Vite.js frontend project, clone it first using:
+
+```bash
+git clone https://github.com/bryntum/bryntum-calendar-mongodb-vanilla.git
+```
+Next, install the dependencies using:
+
+```bash
+npm install`
+```
--- src/main.js
+++ src/main.js
@@ -1,25 +1,7 @@
 import './style.css';
-import { Calendar } from '@bryntum/calendar';
 
-const calendar = new Calendar({
-
-    appendTo : 'app',
-    date : '2025-10-05',
-
-    resources : [
-        {
-            id         : 1,
-            name       : 'Default Calendar',
-            eventColor : 'green'
-        }
-    ],
-    events : [
-        {
-            id         : 1,
-            name       : 'Meeting',
-            startDate  : '2025-10-05T10:00:00',
-            endDate    : '2025-10-05T11:00:00',
-            resourceId : 1
-        }
-    ]
-});
+document.querySelector('#app').innerHTML = `
+  <h2>
+    Bryntum Calendar App with MongoDB
+  </h2>
+`
