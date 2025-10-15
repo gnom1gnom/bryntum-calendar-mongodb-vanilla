56a074614060d53bf4d17d4a4c72e69e1e1a3b29
Krzysztof Jurkowski <krzysztof.jurkowski@roche.com>
2025-10-15 16:29:06 +0200

    Initialize Bryntum Calendar with default resources and events

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
+import { Calendar } from '@bryntum/calendar';
 import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <h2>
-    Bryntum Calendar App with MongoDB
-  </h2>
-`
+const calendar = new Calendar({
+
+  appendTo: 'app',
+  date: '2026-07-01',
+
+  resources: [
+    {
+      id: 1,
+      name: 'Default Calendar',
+      eventColor: 'green'
+    }
+  ],
+  events: [
+    {
+      id: 1,
+      name: 'Meeting',
+      startDate: '2026-07-01T10:00:00',
+      endDate: '2026-07-01T11:00:00',
+      resourceId: 1
+    }
+  ]
+});
